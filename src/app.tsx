eea1f6e33c1bfa6619d0fd884273b68ff1f67c1f
puppypaduk2 <debraooclark@gmail.com>
2021-04-16 18:08:06 +0300

    feat: Add forms

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,27 @@
+import { Button, Input, Space } from 'antd';
+import { useStateGlobal } from 'libs/hooks/use-state-global';
 import React, { FC } from 'react';
 
 export const App: FC = () => {
   return (
     <div>
-      Users
+      <div>Users #1</div>
+      <Form />
+      <div>Users #2</div>
+      <Form />
     </div>
+  );
+};
+
+const Form: FC = () => {
+  const [login, setLogin] = useStateGlobal("", "login");
+  const [pass, setPass] = useStateGlobal("", "password");
+
+  return (
+    <Space direction="vertical">
+      <Input placeholder="Login" value={login} onChange={(event) => setLogin(event.currentTarget.value)} />
+      <Input placeholder="Password" type="password" value={pass} onChange={(event) => setPass(event.currentTarget.value)}  />
+      <Button block type="primary">Send</Button>
+    </Space>
   );
 };
